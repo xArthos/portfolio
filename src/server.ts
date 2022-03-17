6c82294ab4b272b313405180d2cd5857e3d7a3d5
Giampaolo <giampaololocascio@gmail.com>
2022-03-17 17:31:18 +0100

    Test

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,7 @@ import * as http from 'http';
 import express from 'express';
 import { ApolloServer, ExpressContext } from 'apollo-server-express';
 import {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageProductionDefault,
-  ApolloServerPluginLandingPageLocalDefault,
-  ApolloServerPluginInlineTrace
+    ApolloServerPluginLandingPageLocalDefault
 } from 'apollo-server-core';
 
 // GraphQl import
@@ -24,64 +21,60 @@ import 'dotenv/config';
 
 // const startApolloServer = async (schema: any, createTestContext: any) => {
 const startApolloServer = async (schema: any) => {
-  const app: express.Application = (module.exports = express());
-  const httpServer: http.Server = http.createServer(app);
+    const app: express.Application = (module.exports = express());
+    const httpServer: http.Server = http.createServer(app);
 
-  // Connect to MongoDb
-  await initDb();
+    // Connect to MongoDb
+    await initDb();
 
-  // Create an Apollo server
-  const server: ApolloServer<ExpressContext> = new ApolloServer({
-    schema,
-    // context: ({ req }) => {
-    //   const token = req.headers.authorization;
-    //   // TODO: #1 Finish the context
-    //   const currentUser = User.getUserByToken(token);
-    //   return { user: currentUser, User, Message }
-    // },
-    plugins: [
-      ApolloServerPluginInlineTrace(),
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      process.env.NODE_ENV === 'production'
-        ? ApolloServerPluginLandingPageProductionDefault({
-          graphRef: "Graph-Portfolio",
-          footer: false
-        })
-        : ApolloServerPluginLandingPageLocalDefault({ footer: false })
-    ]
-  });
+    // Create an Apollo server
+    const server: ApolloServer<ExpressContext> = new ApolloServer({
+        schema,
+        // context: ({ req }) => {
+        //   const token = req.headers.authorization;
+        //   // TODO: #1 Finish the context
+        //   const currentUser = User.getUserByToken(token);
+        //   return { user: currentUser, User, Message }
+        // },
+        plugins: [
+            ApolloServerPluginLandingPageLocalDefault({ footer: false })
+        ]
+    });
 
-  // Add a list of allowed origins.
-  // If you have more origins you would like to add, you can add them to the array below.
-  // const allowedOrigins = [process.env.CROSS_ORIGIN || 'http://localhost:3000', 'https://studio.apollographql.com', 'https://serverxarthos.vercel.app'];
+    // Add a list of allowed origins.
+    // If you have more origins you would like to add, you can add them to the array below.
+    const allowedOrigins = [process.env.CROSS_ORIGIN || 'http://localhost:3000', 'https://studio.apollographql.com', 'https://serverxarthos.vercel.app'];
 
-  // const corsOptions = {
-  //   origin: allowedOrigins,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   preflightContinue: false,
-  //   // credentials: true,
-  //   optionsSuccessStatus: 204
-  // };
+    const corsOptions: cors.CorsOptions = {
+        origin: allowedOrigins,
+        methods: ['GET', 'PUT', 'POST'],
+        preflightContinue: false,
+        credentials: true,
+        optionsSuccessStatus: 204,
+        maxAge: 84600
+    };
 
-  // app.use(cors(corsOptions));
-  // app.use(express.json());
+    // app.use((req, resp, next) => {
+    //   next()
+    // }, cors(corsOptions))
 
-  app.use(cors());
+    app.use(cors(corsOptions));
+    app.use(express.json());
 
-  // Routes
-  app.get(`/foo`, (req, res) => res.send(`hey`));
+    // Routes
+    app.get(`/`, (req, res) => res.send(`hey`));
 
-  // Run the server
-  consoleMessage('Server', 'startApolloServer', `Attempt to run server`);
+    // Run the server
+    consoleMessage('Server', 'startApolloServer', `Attempt to run server`);
 
-  await server.start();
-  server.applyMiddleware({ app, cors: true });
-  await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
+    await server.start();
+    server.applyMiddleware({ app, cors: true });
+    await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve));
 
-  // Console a successfully response
-  consoleMessageResult(true, 'startApolloServer', `🚀 Server ready at`);
-  console.log('\x1b[34m%s\x1b[0m', `http://localhost:4000${server.graphqlPath}`);
-  console.log('\x1b[90m%s\x1b[0m', '--------------------------');
+    // Console a successfully response
+    consoleMessageResult(true, 'startApolloServer', `🚀 Server ready at`);
+    console.log('\x1b[34m%s\x1b[0m', `http://localhost:4000${server.graphqlPath}`);
+    console.log('\x1b[90m%s\x1b[0m', '--------------------------');
 };
 
 // Start the server
