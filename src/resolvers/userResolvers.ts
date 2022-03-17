fe4b1388bef291c51bcf785175439061e3d0a69d
Giampaolo <giampaololocascio@gmail.com>
2022-03-17 20:50:35 +0100

    Test

--- src/resolvers/userResolvers.ts
+++ src/resolvers/userResolvers.ts
@@ -9,7 +9,7 @@ export const getUser = async (_: any, { _id }: any) => {
     console.log(_id)
     console.log(db)
     try {
-        return await db.collection('users').findOne({ _id: new ObjectId(_id) });;
+        return await db.collection('users').findOne({ _id: new ObjectId(_id ? _id : '623222d2826ad9c729d5fb1e') });;
     } catch (error: any) {
         throw new ApolloError(error);
     } finally {
