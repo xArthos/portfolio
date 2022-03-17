c4e498407c12863a5b9329152487d754f616f597
Giampaolo <giampaololocascio@gmail.com>
2022-03-17 14:11:04 +0100

    Updating cors options

--- src/server.ts
+++ src/server.ts
@@ -63,6 +63,17 @@ const startApolloServer = async (schema: any) => {
 
   app.use(cors(corsOptions));
   app.use(express.json());
+  app.use((req, res, next) => {
+    const origin: any = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    return next();
+  });
 
   // Routes
   app.get(`/foo`, (req, res) => res.send(`hey`));
