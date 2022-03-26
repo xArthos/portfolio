9047b1ee6ac144966b344061fb4beaad423890f1
Giampaolo <giampaololocascio@gmail.com>
2022-03-26 15:39:13 +0100

    Test

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,7 @@ import 'dotenv/config';
 // Add a list of allowed origins.
 // If you have more origins you would like to add, you can add them to the array below.
 const allowedOrigins = [
-    'http://localhost',
+    'http://localhost:3000',
     'https://studio.apollographql.com'
 ];
 
